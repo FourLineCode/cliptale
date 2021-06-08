b424aa1e9aefbca299283eb124f78f6c9b2d6a88
FourLineCode <akmal3535.ah@gmail.com>
2021-06-09 01:50:05 +0600

    init react+tailwindcss

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const App = () => {
+	return (
+		<main className='flex items-center justify-center w-screen h-screen'>
+			<h1 className='text-4xl font-bold text-green-500'>Hello ClipTale!</h1>
+		</main>
+	);
+};
---
+++ webpack.renderer.config.js
@@ -0,0 +1,28 @@
+const rules = require('./webpack.rules');
+const plugins = require('./webpack.plugins');
+
+rules.push({
+	test: /\.css$/,
+	use: [
+		{ loader: 'style-loader' },
+		{ loader: 'css-loader' },
+		{
+			loader: 'postcss-loader',
+			options: {
+				postcssOptions: {
+					plugins: ['postcss-preset-env'],
+				},
+			},
+		},
+	],
+});
+
+module.exports = {
+	module: {
+		rules,
+	},
+	plugins: plugins,
+	resolve: {
+		extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+	},
+};
