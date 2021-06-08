d0b9d10cdbd9c90f33f5442a5e7fc65b655393c0
FourLineCode <akmal3535.ah@gmail.com>
2021-06-09 05:59:15 +0600

    context menu, tray icons and stuff

---
+++ src/internal/config.ts
@@ -0,0 +1,20 @@
+import { app } from 'electron';
+import path from 'path';
+
+export const config = {
+	dev: process.env.NODE_ENV === 'development',
+	window: {
+		initialShow: false,
+		width: 400,
+		height: 500,
+		resizable: false,
+		frame: false,
+		menu: null,
+		center: true,
+		fullscreenable: false,
+		movable: false,
+		rounded: true,
+	},
+	appIcon: path.join(app.getAppPath(), '/src/assets/clipboard@8x.png'),
+	trayIcon: path.join(app.getAppPath(), '/src/assets/clipboard@1x.png'),
+};
