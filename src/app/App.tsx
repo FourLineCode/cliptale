34d28b0ba89db7eadab83b270df7d7a0a45ac113
FourLineCode <akmal3535.ah@gmail.com>
2021-06-09 21:18:06 +0600

    oop design init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const App = () => {
 	return (
-		<main className='flex items-center justify-center w-screen h-screen'>
+		<main className='flex items-center justify-center w-screen h-screen border-4 border-green-800'>
 			<h1 className='text-4xl font-bold text-green-500'>Hello ClipTale!</h1>
 		</main>
 	);
--- src/internal/config.ts
+++ src/internal/config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 export const config = {
 	dev: process.env.NODE_ENV === 'development',
 	window: {
-		initialShow: false,
+		initialShow: true,
 		width: 400,
 		height: 500,
 		resizable: false,
@@ -14,7 +14,10 @@ export const config = {
 		fullscreenable: false,
 		movable: false,
 		rounded: true,
+		hideMenu: true,
 	},
 	appIcon: path.join(app.getAppPath(), '/src/assets/clipboard@8x.png'),
 	trayIcon: path.join(app.getAppPath(), '/src/assets/clipboard@1x.png'),
 };
+
+export type Config = typeof config;
